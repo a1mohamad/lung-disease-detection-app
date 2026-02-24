95814b5e48c4c9f64d2b6a2ca9e795923a2da064
a1mohamad <a1.mohamad.askari@gmail.com>
2026-02-24 03:46:13 +0330

    mlops: new package constructor

--- deployment/mlops/core/data/__init__.py
+++ deployment/mlops/core/data/__init__.py
@@ -3,4 +3,9 @@ from mlops.core.data.datasets import (
     build_multiclass_dataset,
     build_segmentation_dataset,
 )
-from mlops.core.data.tfrecord_ops import list_tfrecords, split_tfrecords
+from mlops.core.data.tfrecord_ops import (
+    compute_steps_from_tfrecords,
+    count_examples_in_tfrecords,
+    list_tfrecords,
+    split_tfrecords,
+)
