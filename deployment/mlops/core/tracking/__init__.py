3a5eb460be8234276a1c8aa9fba15f2624815829
a1mohamad <a1.mohamad.askari@gmail.com>
2026-02-24 23:20:13 +0330

    mlops: initial file for package now contain summary functions

--- deployment/mlops/core/tracking/__init__.py
+++ deployment/mlops/core/tracking/__init__.py
@@ -1,5 +1,6 @@
-from mlops.core.tracking.mlflow_io import (
+﻿from mlops.core.tracking.mlflow_io import (
     flatten_dict,
     load_yaml,
 )
 from mlops.core.tracking.registry import get_best_production_metric, get_client, load_model_from_registry, promote_if_better
+from mlops.core.tracking.run_summary import build_run_summary, log_run_summary
