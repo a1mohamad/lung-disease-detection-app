dd323986c142f519bc190dc5f6d423de68e75cf5
a1mohamad <a1.mohamad.askari@gmail.com>
2026-02-27 02:56:16 +0330

    mlops/airflow: change runtime shcedule of segmentation dag

--- deployment/mlops/airflow/dags/retrain_segmentation_unet_xception.py
+++ deployment/mlops/airflow/dags/retrain_segmentation_unet_xception.py
@@ -18,7 +18,7 @@ from mlops.airflow.tasks.retrain import retrain_model
 
 with DAG(
     dag_id="retrain_unet_xception_segmentation",
-    start_date=datetime(2026, 2, 1),
+    start_date=datetime(2026, 3, 1),
     schedule="@monthly",
     catchup=False,
     tags=["mlflow", "retrain", "segmentation"],
@@ -38,3 +38,5 @@ with DAG(
             "val_ratio": VAL_RATIO,
         },
     )
+
+
