1d14803fc623ba32c2c67b41ef6b26f24c95ea64
a1mohamad <a1.mohamad.askari@gmail.com>
2026-05-28 22:43:15 +0330

    powershell: mlops sh file decparated

--- deployment/mlops.ps1
+++ deployment/mlops.ps1
@@ -1,36 +1,9 @@
 param(
     [Parameter(Mandatory = $true)]
-    [ValidateSet(
-        "mlops-build",
-        "mlops-up",
-        "mlops-down",
-        "mlops-logs",
-        "mlops-ps",
-        "backfill",
-        "final-build",
-        "final-up",
-        "final-down",
-        "final-logs",
-        "final-ps"
-    )]
     [string]$Task
 )
 
-$envFile = ".env.compose"
-$mlopsFile = "docker-compose.mlops.yml"
-$finalFile = "docker-compose.final.yml"
+$ErrorActionPreference = "Stop"
 
-switch ($Task) {
-    "mlops-build" { docker compose --progress=plain --env-file $envFile -f $mlopsFile build }
-    "mlops-up" { docker compose --env-file $envFile -f $mlopsFile up -d db db-init mlflow airflow }
-    "mlops-down" { docker compose --env-file $envFile -f $mlopsFile down --remove-orphans }
-    "mlops-logs" { docker compose --env-file $envFile -f $mlopsFile logs -f }
-    "mlops-ps" { docker compose --env-file $envFile -f $mlopsFile ps }
-    "backfill" { docker compose --env-file $envFile -f $mlopsFile --profile manual run --rm post_hoc_backfill }
-    "final-build" { docker compose --progress=plain --env-file $envFile -f $finalFile build }
-    "final-up" { docker compose --env-file $envFile -f $finalFile up -d }
-    "final-down" { docker compose --env-file $envFile -f $finalFile down --remove-orphans }
-    "final-logs" { docker compose --env-file $envFile -f $finalFile logs -f }
-    "final-ps" { docker compose --env-file $envFile -f $finalFile ps }
-}
-
+Write-Warning "mlops.ps1 is kept for compatibility. Prefer .\docker.ps1 <task>."
+& "$PSScriptRoot\docker.ps1" $Task
