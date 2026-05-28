1a8c8faa4e3b987816e7ff11715004faba8125ed
a1mohamad <a1.mohamad.askari@gmail.com>
2026-05-28 20:10:41 +0330

    db: condition for db if defined in .env

--- deployment/app/db/session.py
+++ deployment/app/db/session.py
@@ -4,24 +4,32 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from app.configs.config import AppConfig
+from app.utils.errors import ServiceError
 
 
-engine = create_engine(
-    AppConfig.get_database_url(),
-    echo=AppConfig.DB_ECHO,
-    pool_pre_ping=True,
-    future=True,
-)
+engine = None
+SessionLocal = None
 
-SessionLocal = sessionmaker(
-    bind=engine,
-    autoflush=False,
-    autocommit=False,
-    future=True,
-)
+if AppConfig.DB_LOGGING_ENABLED:
+    engine = create_engine(
+        AppConfig.get_database_url(),
+        echo=AppConfig.DB_ECHO,
+        pool_pre_ping=True,
+        future=True,
+    )
+
+    SessionLocal = sessionmaker(
+        bind=engine,
+        autoflush=False,
+        autocommit=False,
+        future=True,
+    )
 
 
 def get_db() -> Generator:
+    if SessionLocal is None:
+        raise ServiceError("DB_DISABLED", "Database logging is disabled.")
+
     db = SessionLocal()
     try:
         yield db
