d038cf2a9d56fbc5eb1c4b74e7e574bdc3954e24
a1mohamad <a1.mohamad.askari@gmail.com>
2026-06-22 21:40:46 +0330

    mlops: add __init__ file for public declaration

---
+++ deployment/mlops/core/ingestion/__init__.py
@@ -0,0 +1,8 @@
+from mlops.core.ingestion.manifest import ReviewedBatch, ReviewedRecord
+from mlops.core.ingestion.snapshot import prepare_reviewed_snapshot
+
+__all__ = [
+    "ReviewedBatch",
+    "ReviewedRecord",
+    "prepare_reviewed_snapshot",
+]
